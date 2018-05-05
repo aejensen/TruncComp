1ea7b40fcb836dc6164c22f25216d962d9b84531
Andreas Jensen <aeje@sund.ku.dk>
2018-05-05 10:01:03 +0200

    changing simulations

--- coverageSimulation.R
+++ coverageSimulation.R
@@ -35,37 +35,36 @@ simCoverage <- function(nsim, n, scenario, method, ncores, seed=12345) {
     inclusion.OR <- (m$alphaDeltaCI[1] < orDeltaTrue) & (m$alphaDeltaCI[2] > orDeltaTrue)
 
     #Get joint surface (slow!)
-    jointSurface <- jointContrastCI(m, muDelta = seq(m$muDeltaCI[1] -0.4 , m$muDeltaCI[2] + 0.4, length.out = 10),
-                                       logORdelta = seq(log(m$alphaDeltaCI[1]) - 0.4, log(m$alphaDeltaCI[2]) + 0.4, length.out = 10),
-                                       plot=TRUE)
+    #jointSurface <- jointContrastCI(m, muDelta = seq(m$muDeltaCI[1] -0.4 , m$muDeltaCI[2] + 0.4, length.out = 5),
+    #                                   logORdelta = seq(log(m$alphaDeltaCI[1]) - 0.4, log(m$alphaDeltaCI[2]) + 0.4, length.out = 5),
+    #                                   plot=TRUE)
 
     #Get joint inclusion indicator
-    jointContour <- contourLines(jointSurface$muDelta, jointSurface$logORdelta, jointSurface$surface, levels=stats::qchisq(0.95, 2))
-    inclusion.joint <- sp::point.in.polygon(muDeltaTrue, log(orDeltaTrue), jointContour[[1]]$x, jointContour[[1]]$y)
+    #jointContour <- contourLines(jointSurface$muDelta, jointSurface$logORdelta, jointSurface$surface, levels=stats::qchisq(0.95, 2))
+    #inclusion.joint <- sp::point.in.polygon(muDeltaTrue, log(orDeltaTrue), jointContour[[1]]$x, jointContour[[1]]$y)
+    inclusion.joint <- 1
 
     c(m$muDelta, log(m$alphaDelta), inclusion.muDelta, inclusion.OR, as.numeric(inclusion.joint == 1))
   }, mc.cores=ncores, mc.preschedule = FALSE)
 
-  colnames(out) <- c("muDelta", "logOR", "covMuDelta", "covLogOR", "covJoint")
   out
 }
 
-cov.1.SPLRT.50 <- simCoverage(nsim = nSim, n = 200, scenario = 1, method="SPLRT", ncores=64)
-
-
-  out <- out[sapply(out, is.vector)]
-  out <- do.call("rbind", out)
-
-  muDelta <- mean(out[,1], na.rm=TRUE)
-  alphaDelta <- mean(out[,2], na.rm=TRUE)
-  cov.muDelta <- mean(out[,3] == 1, na.rm=TRUE)
-  cov.OR <- mean(out[,4] == 1, na.rm=TRUE)
-  cov.simult <- mean(out[,5] == 1, na.rm=TRUE)
-
-  c(muDelta, alphaDelta, muDeltaCov = cov.muDelta, logORcov = cov.OR, simultCov = cov.simult)
+getStats <- function(res, muDelta, logOR) {
+  mat <- do.call("rbind", res)
+  out <- c(biasMuDelta = mean(mat[,1] - muDelta, na.rm=TRUE),
+           sdMuDelta = sd(mat[,1], na.rm=TRUE),
+           biasLogOR = mean(mat[,2] - logOR),
+           sdLogOR = sd(mat[,1]),
+           covMuDelta = mean(mat[,3]),
+           covLogOR = mean(mat[,4]),
+           covJoint = mean(mat[,5]))
+  round(out, 4)
 }
 
-nSim <- 2000
+nSim <- 1000
+
+#nSim <- 2000
 
 cov.1.SPLRT.50 <- simCoverage(nsim = nSim, n = 50, scenario = 1, method="SPLRT", ncores=64)
 cov.2.SPLRT.50 <- simCoverage(nsim = nSim, n = 50, scenario = 2, method="SPLRT", ncores=64)
@@ -79,24 +78,41 @@ cov.3.SPLRT.100 <- simCoverage(nsim = nSim, n = 100, scenario = 3, method="SPLRT
 cov.1.SPLRT.200 <- simCoverage(nsim = nSim, n = 200, scenario = 1, method="SPLRT", ncores=64)
 cov.2.SPLRT.200 <- simCoverage(nsim = nSim, n = 200, scenario = 2, method="SPLRT", ncores=64)
 cov.3.SPLRT.200 <- simCoverage(nsim = nSim, n = 200, scenario = 3, method="SPLRT", ncores=64)
+cov.4.SPLRT.200 <- simCoverage(nsim = nSim, n = 200, scenario = 4, method="SPLRT", ncores=64)
 
 
 
-cov.1.SPLRT.50
-cov.2.SPLRT.50
-cov.3.SPLRT.50
+getStats(cov.1.SPLRT.50, 1, log(1))
+getStats(cov.2.SPLRT.50, 0, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
+getStats(cov.3.SPLRT.50, 1, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
 
-cov.1.SPLRT.100
-cov.2.SPLRT.100
-cov.3.SPLRT.100
+getStats(cov.4.SPLRT.50, 1, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
 
-cov.1.SPLRT.200
-cov.2.SPLRT.200
-cov.3.SPLRT.200
+getStats(cov.1.SPLRT.100, 1, log(1))
+getStats(cov.2.SPLRT.100, 0, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
+getStats(cov.3.SPLRT.100, 1, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
+
+getStats(cov.1.SPLRT.200, 1, log(1))
+getStats(cov.2.SPLRT.200, 0, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
+getStats(cov.3.SPLRT.200, 1, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
+getStats(cov.4.SPLRT.200, 1, log((0.3/(1-0.3)) / (0.4/(1-0.4))))
 
 save.image("coverageResults.RData")
 
+#################
+data <- TruncComp:::simTruncData(200, 0, 1, 0.3, 0.4, dist="t-sq")
+m <- truncComp.default(data$Y, data$A, data$R, method="SPLRT")
+m
 
-n <- 1000
-data <- TruncComp:::simTruncData(n, 3, 4, 1, 1, dist="t-sq")
+
+mean(data$Y[data$A == 1 & data$R == 0])
+mean(data$Y[data$A == 1 & data$R == 1])
+
+mean(data$Y[data$A == 1 & data$R == 0]) - mean(data$Y[data$A == 1 & data$R == 1])
 hist(data$Y)
+
+
+
+
+
+hist(stats::rt(1000, df = 3)^2 + 2)
